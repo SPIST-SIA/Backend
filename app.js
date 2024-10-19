d6a7a807ce3102a4ec4e7fe12f0312e5e824451d
Cjanniverson <c18-4293-01@spist.edu.ph>
2024-10-19 12:29:15 +0800

    Update

--- app.js
+++ app.js
@@ -13,5 +13,22 @@ app.use('/users', UserRouter)
 const InstructorsRouter = require('./routes/instructors')
 app.use('/instructors', InstructorsRouter)
 
+// ================== Get all students ===================== //
+app.get('/students', (req, res) => {
+    db.query('SELECT * FROM students', (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });
+  
+  // ====================== Get all student info ====================== //
+  app.get('/studentinfo', (req, res) => {
+    db.query('SELECT * FROM studentinfo', (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });
+
+  
 
 app.listen(5000, () => console.log('server is running at 127.0.0.1:5000'))
