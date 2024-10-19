72e7257409b0c233da0ae4e02ee1d63c10ac3d74
Cjanniverson <c18-4293-01@spist.edu.ph>
2024-10-19 12:17:55 +0800

    Update

--- routes/instructors.js
+++ routes/instructors.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {getInstructors, createInstructors, deleteInstructors} = require('../controllers/instructors.controller.js');
+const {getInstructors, createInstructors} = require('../controllers/instructors.controller.js');
 
 router.get('/', getInstructors);
 router.post('/', createInstructors);
-router.delete('/:id', deleteInstructors.deleteInstructorsById);
+
 module.exports = router;
